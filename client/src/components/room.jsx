95bc20e8491fc816bb38f60401566ef2e4195580
Afam <78809835+neon-gpt@users.noreply.github.com>
2023-11-08 23:38:20 +0000

    added functionality to join private rooms

--- client/src/components/room.jsx
+++ client/src/components/room.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Room = ({room}) => {
 
   const navigate = useNavigate();
+  const [passcode, setPasscode] = useState(null);
 
-  const joinRoom = async () => {
+  const joinPublicRoom = async () => {
     const response = await fetch("http://localhost:8080/api/rooms/joinroom", {
       method: "POST",
       headers: {
@@ -13,7 +14,6 @@ const Room = ({room}) => {
       },
       body: JSON.stringify({
         "roomName": `${room.roomName}`,
-        "roomPasscode": `${room.roomPasscode}`
       })
     });
 
@@ -22,12 +22,36 @@ const Room = ({room}) => {
     }
   } 
 
+  const joinPrivateRoom = async () => {
+    const response = await fetch("http://localhost:8080/api/rooms/joinroom", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "roomName": `${room.roomName}`,
+        "roomPasscode": `${passcode}`
+      })
+    });
+
+    if (response.ok) {
+      navigate(`/codespace/${room.id}`);
+    }
+
+  }
+
   return (
     <div>
       <h1> {room.roomName} </h1>
       <h1> {room.roomUsers} </h1>
       <h1> {room.roomType} </h1>
-      <button onClick={joinRoom}> Join Room </button>
+      <button onClick={room.roomType === "Public" ? joinPublicRoom : joinPrivateRoom}> Join Room </button>
+      {room.roomType === "Private" &&
+        <input
+          onChange = {(e) => setPasscode(e.target.value)}
+          className="text-black"
+        />
+      }
     </div>
   )
 }
